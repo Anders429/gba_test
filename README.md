d2169ad3f0d1484d0f6ba80f63cff8344f9a9b6a
Anders429 <anders.stefan.evensen@gmail.com>
2023-05-03 00:44:21 -0700

    Building out the trial reporting logic.

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ Currently a work-in-progress.
 - Compatibility with custom test runners for specific environments.
 - Display test results on screen.
 - Feature parity with Rust's built-in testing.
+
+## Strategy
+Tests will be run single-threaded, recovering after panics. The test results will be serialized directly to external memory. This will allow writing a max of 64 KiB of data, so a serialization format that is very concise will need to be used.
+
+`bincode` is probably the most straight-forward solution. The upcoming version 2.0 supports `no_std` environments, and still interoperates with `serde`. However, it's questionable whether they will continue supporting `serde` as a first-class citizen, as they've introduced their own traits to seemingly replace `serde`'s. This project should stick to `serde` if possible, as that allows easy switching of the serialization in the future, and `serde` is easy to customize. At this point, it is not clear to me how to customize `bincode` in the same ways.
+
+Another alternative is `postcard`. When making this decision, it really comes down to what I can use to serialize the smallest amount of data for a test result.
+
+After each test is run, the result can be written directly to SRAM. If displaying is enabled, the result will also be written to the screen.
+
+After all tests are run, display mode can display the number of tests that passed and failed, and allow a user to scroll through them.
+
+The format of data written to SRAM should also include the number of tests run at the beginning. This can be known ahead of time by simply examining the `tests` array provided to the test runner.
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #![no_std]
 
+use core::{fmt, panic::PanicInfo};
+use serde::{ser::{Serialize, Serializer, SerializeStruct}, de, de::{Deserialize, Deserializer, SeqAccess, Unexpected, Visitor}};
+
 /// The remaining tests to be run.
 static mut TESTS: &[&dyn TestCase] = &[];
 
@@ -15,11 +18,98 @@ pub trait TestCase {
     fn run(&self);
 }
 
+/// The outcome of a test.
+#[derive(Clone, Copy, Debug)]
+pub enum Outcome {
+    Passed,
+    Failed,
+}
+
+impl Serialize for Outcome {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        serializer.serialize_u8(*self as u8)
+    }
+}
+
+impl<'de> Deserialize<'de> for Outcome {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+        struct OutcomeVisitor;
+
+        impl<'de> Visitor<'de> for OutcomeVisitor {
+            type Value = Outcome;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a byte containing the value 0 or 1")
+            }
+
+            fn visit_u8<E>(self, value: u8) -> Result<Self::Value, E> where E: de::Error {
+                match value {
+                    0 => Ok(Outcome::Passed),
+                    1 => Ok(Outcome::Failed),
+                    _ => Err(E::invalid_value(Unexpected::Unsigned(value.into()), &self)),
+                }
+            }
+        }
+
+        deserializer.deserialize_u8(OutcomeVisitor)
+    }
+}
+
+/// A single test result.
+#[derive(Debug)]
+pub struct Trial<'a> {
+    /// The name of the test.
+    pub name: &'a str,
+    /// The test's outcome.
+    pub outcome: Outcome,
+}
+
+impl<'a> Serialize for Trial<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        let mut trial = serializer.serialize_struct("Trial", 2)?;
+
+        trial.serialize_field("name", self.name)?;
+        trial.serialize_field("outcome", &self.outcome)?;
+
+        trial.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for Trial<'de> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+        struct TrialVisitor;
+
+        impl<'de> Visitor<'de> for TrialVisitor {
+            type Value = Trial<'de>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("struct Trial")
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error> where A: SeqAccess<'de> {
+                let name = seq.next_element()?.ok_or(de::Error::missing_field("name"))?;
+                let outcome = seq.next_element()?.ok_or(de::Error::missing_field("outcome"))?;
+
+                Ok(Trial {
+                    name,
+                    outcome,
+                })
+            }
+        }
+
+        deserializer.deserialize_struct("Trial", &["name", "outcome"], TrialVisitor)
+    }
+}
+
+fn report_test_result(outcome: Outcome) {
+    todo!("write the trial to the next location in SRAM")
+}
+
 /// Runs the remaining tests.
 /// 
 /// The current test being executed is tracked using global state. This allows the runner to
 /// recover when a test panics.
-fn run_tests() {
+fn run_tests() -> ! {
     // SAFETY: `TESTS` is only ever mutated on the main thread.
     while let Some((test, tests)) = unsafe {TESTS}.split_first() {
         // SAFETY: `TESTS` is only ever mutated on the main thread.
@@ -28,6 +118,13 @@ fn run_tests() {
         }
         test.run();
     }
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    loop {}
 }
 
 /// A test runner to execute tests as a Game Boy Advance ROM.
