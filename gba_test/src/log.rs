00a74f31b2a57a81323f32ed26bdf73256a68504
Anders429 <anders.stefan.evensen@gmail.com>
2024-06-07 12:55:46 -0600

    Document log module.

--- gba_test/src/log.rs
+++ gba_test/src/log.rs
@@ -1,3 +1,10 @@
+//! Facades for logging.
+//!
+//! These are equivalent to using `log`'s macros directly, but they only expand if the `log`
+//! feature is enabled.
+//!
+//! Only the macros used in this framework are provided here. More can be added as needed.
+
 macro_rules! info {
     ($($tokens:tt)*) => {
         #[cfg(feature = "log")]
