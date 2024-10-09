f2539b408129644b0e3989dea1dc5627888acd47
Anders429 <anders.stefan.evensen@gmail.com>
2024-10-09 16:25:15 -0600

    Use global asm instead of naked functions for runtime.

--- gba_test/src/lib.rs
+++ gba_test/src/lib.rs
@@ -73,7 +73,6 @@
 //! potentially breaking this framework.
 
 #![no_std]
-#![feature(naked_functions)]
 #![cfg_attr(test, no_main)]
 #![cfg_attr(test, feature(custom_test_frameworks))]
 #![cfg_attr(test, test_runner(runner))]
